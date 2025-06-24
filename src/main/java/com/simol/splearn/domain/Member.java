d40fdba1b7632ac3f822405e358c03d0f5ff5853
juno-choi <ililil9482@naver.com>
2025-06-24 11:24:32 +0900

    Member 도메인 기능 정의

--- src/main/java/com/simol/splearn/domain/Member.java
+++ src/main/java/com/simol/splearn/domain/Member.java
@@ -3,6 +3,8 @@ package com.simol.splearn.domain;
 import lombok.Getter;
 import org.springframework.util.Assert;
 
+import java.util.Objects;
+
 @Getter
 public class Member {
     private String email;
@@ -13,11 +15,15 @@ public class Member {
 
     private MemberStatus status;
 
-    public Member(String email, String nickname, String passwordHash) {
-        this.email = email;
-        this.nickname = nickname;
-        this.passwordHash = passwordHash;
+    private Member(String email, String nickname, String passwordHash) {
+        this.email = Objects.requireNonNull(email);
+        this.nickname = Objects.requireNonNull(nickname);
+        this.passwordHash = Objects.requireNonNull(passwordHash);
         this.status = MemberStatus.PENDING;
+    }
+
+    public static Member create(String email, String nickname, String password, PasswordEncoder passwordEncoder) {
+        return new Member(email, nickname, passwordEncoder.encode(password));
     }
 
     public void activate() {
@@ -28,5 +34,17 @@ public class Member {
     public void deactivate() {
         Assert.state(this.status == MemberStatus.ACTIVATE, "ACTIVE 상태가 아닙니다");
         this.status = MemberStatus.DEACTIVATED;
+    }
+
+    public boolean verifyPassword(String secret, PasswordEncoder passwordEncoder) {
+        return passwordEncoder.matches(secret, this.passwordHash);
+    }
+
+    public void changeNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void changePassword(String password, PasswordEncoder passwordEncoder) {
+        this.passwordHash = passwordEncoder.encode(password);
     }
 }
--- src/test/java/com/simol/splearn/domain/MemberTest.java
+++ src/test/java/com/simol/splearn/domain/MemberTest.java
@@ -1,15 +1,35 @@
 package com.simol.splearn.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberTest {
+    // fixture
+    Member member;
+
+    PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setUp() {
+        this.passwordEncoder = new PasswordEncoder() {
+            @Override
+            public String encode(String password) {
+                return password.toUpperCase();
+            }
+
+            @Override
+            public boolean matches(String password, String passwordHash) {
+                return encode(password).equals(passwordHash);
+            }
+        };
+        member = Member.create("mail@mail.com", "juno", "secret", passwordEncoder);
+    }
 
     @Test
     void activateFail1() {
-        Member member = new Member("<EMAIL>", "test", "test");
         member.activate();
 
         Assertions.assertThatThrownBy(() -> {
@@ -19,7 +39,6 @@ class MemberTest {
 
     @Test
     void activateSuccess() {
-        Member member = new Member("<EMAIL>", "test", "test");
         member.activate();
 
         Assertions.assertThat(member.getStatus()).isEqualTo(MemberStatus.ACTIVATE);
@@ -27,11 +46,32 @@ class MemberTest {
 
     @Test
     void deactivateFail1() {
-        Member member = new Member("<EMAIL>", "test", "test");
-
         Assertions.assertThatThrownBy(() -> {
             member.deactivate();
         }).isInstanceOf(IllegalStateException.class);
 
+    }
+
+    @Test
+    void verifyPassword() {
+        Assertions.assertThat(member.verifyPassword("secret", passwordEncoder)).isTrue();
+    }
+
+    @Test
+    void changeNickname() {
+        Assertions.assertThat(member.getNickname()).isEqualTo("juno");
+
+        member.changeNickname("junho");
+
+        Assertions.assertThat(member.getNickname()).isEqualTo("junho");
+    }
+
+    @Test
+    void changePassword() {
+        String newPassword = "verysecret";
+
+        member.changePassword(newPassword, passwordEncoder);
+
+        Assertions.assertThat(member.verifyPassword(newPassword, passwordEncoder)).isTrue();
     }
 }
