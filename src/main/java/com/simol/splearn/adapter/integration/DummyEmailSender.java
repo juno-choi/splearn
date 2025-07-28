131f63c3b61aae96a6619c65cd14ecdea2fbadd0
juno-choi <ililil9482@naver.com>
2025-07-28 17:10:50 +0900

    junitpioneer 사용으로 console 찍는 내용 확인 테스트

--- src/main/java/com/simol/splearn/adapter/integration/DummyEmailSender.java
+++ src/main/java/com/simol/splearn/adapter/integration/DummyEmailSender.java
@@ -2,9 +2,12 @@ package com.simol.splearn.adapter.integration;
 
 import com.simol.splearn.application.required.EmailSender;
 import com.simol.splearn.domain.Email;
+import org.springframework.context.annotation.Fallback;
 import org.springframework.stereotype.Component;
 
 @Component
+// spring 6.2에 추가된 기능으로 bean을 모두 검색하다가 등록된 bean이 없다면 @Fallback 을 추가해서 사용한다.
+@Fallback
 public class DummyEmailSender implements EmailSender {
     @Override
     public void send(Email email, String subject, String body) {
---
+++ src/test/java/com/simol/splearn/adapter/integration/DummyEmailSenderTest.java
@@ -0,0 +1,22 @@
+package com.simol.splearn.adapter.integration;
+
+import com.simol.splearn.domain.Email;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.StdIo;
+import org.junitpioneer.jupiter.StdOut;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DummyEmailSenderTest {
+
+    @Test
+    @StdIo
+    void dummyEmailSender(StdOut out) {
+        DummyEmailSender dummyEmailSender = new DummyEmailSender();
+        dummyEmailSender.send(new Email("juno@mail.com"), "subject", "body");
+
+        Assertions.assertThat(out.capturedLines()[0]).isEqualTo("dummy send email: Email[address=juno@mail.com]");
+
+    }
+}
